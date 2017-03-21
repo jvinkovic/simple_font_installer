20b3bf47dfb432784ae5591fbbc388ac96801d19
jvinkovic <josip.vinkovic@span.eu>
2017-03-21 17:21:38 +0100

    finished - cleaning needed (one day...)

---
+++ FoodFontInstaller/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FoodFontInstaller
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label = new System.Windows.Forms.Label();
+            this.labelInstalling = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label.Location = new System.Drawing.Point(34, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(175, 31);
+            this.label.TabIndex = 0;
+            this.label.Text = "Install fonts!";
+            this.label.Click += new System.EventHandler(this.label_Click);
+            // 
+            // labelInstalling
+            // 
+            this.labelInstalling.AutoSize = true;
+            this.labelInstalling.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelInstalling.Location = new System.Drawing.Point(12, 9);
+            this.labelInstalling.Name = "labelInstalling";
+            this.labelInstalling.Size = new System.Drawing.Size(232, 31);
+            this.labelInstalling.TabIndex = 1;
+            this.labelInstalling.Text = "Installing fonts...";
+            this.labelInstalling.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 51);
+            this.Controls.Add(this.labelInstalling);
+            this.Controls.Add(this.label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(270, 90);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(270, 90);
+            this.Name = "MainForm";
+            this.Text = "FoodFontInstaller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label labelInstalling;
+    }
+}
+
---
+++ FoodFontInstaller/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FoodFontInstaller
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void InstallFont(string fontPath)
+        {
+            Shell32.Shell shell = new Shell32.Shell();
+            Shell32.Folder fontFolder = shell.NameSpace(0x14);
+            fontFolder.CopyHere(fontPath);
+        }
+
+        [DllImport("gdi32", EntryPoint = "AddFontResource")]
+        public static extern int AddFontResourceA(string lpFileName);
+
+        [DllImport("gdi32.dll")]
+        private static extern int AddFontResource(string lpszFilename);
+
+        [DllImport("gdi32.dll")]
+        private static extern int CreateScalableFontResource(uint fdwHidden, string
+        lpszFontRes, string lpszFontFile, string lpszCurrentPath);
+
+        /// <summary>
+        /// Installs font on the user's system and adds it to the registry so it's available on the next session
+        /// </summary>
+        /// <param name="contentFontName">Your font to be passed as a resource (i.e. "myfont.tff")</param>
+        private static void RegisterFont(string fontPath, string contentFontName)
+        {
+            // Creates the full path where your font will be installed
+            var fontDestination = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), contentFontName);
+
+            if (!File.Exists(fontDestination))
+            {
+                // Copies font to destination
+                File.Copy(fontPath, fontDestination);
+
+                // Retrieves font name
+                // Makes sure you reference System.Drawing
+                PrivateFontCollection fontCol = new PrivateFontCollection();
+                fontCol.AddFontFile(fontDestination);
+                var actualFontName = fontCol.Families[0].Name;
+
+                //Add font
+                AddFontResource(fontDestination);
+                //Add registry entry
+                Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts", actualFontName, contentFontName, RegistryValueKind.String);
+            }
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            label.Visible = false;
+            labelInstalling.Visible = true;
+
+            foreach (var f in Directory.GetFiles("Fonts"))
+                RegisterFont(f, f.Substring(f.LastIndexOf("\\") + 1));
+
+            MessageBox.Show("Fonts installed!");
+            Close();
+        }
+    }
+}
